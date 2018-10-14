99fa2a8d23ed8499567ce1ba49c49eff3ec9ff4a
witandjest <witjest@gmail.com>
2018-10-14 16:26:37 -0400

    Adding boilerplate code for basic React app

---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import '../styles/Search.css';
+
+class Search extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My React App!</h1>
+            </div>
+        );
+    }
+}
+
+export default Search;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Search from "./components/Search";
+
+ReactDOM.render(
+    <div>
+        <Search />
+    </div>, 
+    document.getElementById("root")
+);
